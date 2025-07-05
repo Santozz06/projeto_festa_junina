90191aa5a75bb602fb7292318cfa2cd892dfd2fe
Milena <sutelmilena@gmail.com>
2025-07-05 20:51:08 -0300

    Telas prontas

--- app/src/main/java/com/example/myapplication/carrinho.java
+++ app/src/main/java/com/example/myapplication/carrinho.java
@@ -10,29 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class carrinho extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_carrinho);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void iratracoes(View view) {
-        Intent i = new Intent(MainActivity.this, atracoes.class);
-        startActivity(i);
-    }
-    public void ircardapio(View view) {
-        Intent i = new Intent(MainActivity.this, atracoes.class);
-        startActivity(i);
-    }
-    public void irfichas(View view) {
-        Intent i = new Intent(MainActivity.this, fichas.class);
+    public void irtelaprincipalll(View view) {
+        Intent i = new Intent(carrinho.this, MainActivity.class);
         startActivity(i);
     }
 }
